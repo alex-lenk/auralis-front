5cf750d29a89efa5310bd328d3a269b1467ac5ed
AlexLenk <a.yankovoy@marvel.ru>
2025-10-25 17:46:42 +0300

    Fixed footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,4 +1,4 @@
-import { NavLink } from 'react-router';
+import { NavLink, useLocation } from 'react-router';
 import { useTranslation } from 'react-i18next';
 import { clsx } from 'clsx';
 import { externalLinks, urlPage } from '@/shared/enum/urlPage';
@@ -7,26 +7,29 @@ import styles from './styles.module.scss';
 
 const Footer = () => {
   const { t } = useTranslation();
+  const { pathname } = useLocation();
   const currentYear = new Date().getFullYear();
-  const hiddenPaths: string[] = [urlPage.Walkman];
 
-  if (hiddenPaths.includes(location.pathname)) {
+  const hiddenPaths: string[] = [urlPage.Walkman];
+  const widthScreen = document.documentElement.clientWidth;
+
+  if (widthScreen < 1200 && hiddenPaths.includes(pathname)) {
     return null;
   }
 
   return (
     <>
-      <footer className={clsx(styles.footer, 'container mx-auto shrink-0 py-4 px-4 mt-auto d:flex')}>
-        <div className={clsx(styles.footerCopy, 'whitespace-pre-line')}>
+      <footer className={clsx(styles.footer, 'container mx-auto shrink-0 py-4 px-4 mt-auto')}>
+        <div className={clsx(styles.footerCopy)}>
           ©{currentYear} <span className="font-accent">Auralis</span> {t('footer.copy')}
-         <NavLink
-          to={externalLinks.AlexLenk}
-          className="font-accent font-bold"
-          target="_blank"
-        >&nbsp;AlexLenk</NavLink>
+          <NavLink
+            to={externalLinks.AlexLenk}
+            className="font-accent font-bold"
+            target="_blank"
+          >&nbsp;AlexLenk</NavLink>
         </div>
 
-        <NavLink className={clsx('font-accent')} to={urlPage.Policy}>
+        <NavLink className="font-accent" to={urlPage.Policy}>
           {t('footer.consent')}
         </NavLink>
       </footer>
